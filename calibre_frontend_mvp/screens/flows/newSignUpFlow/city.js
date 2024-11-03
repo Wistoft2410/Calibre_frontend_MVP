5edee2a8c094289409d30e612992b4a690eb5c41
Adam Isaac <44964494+Wistoft2410@users.noreply.github.com>
2024-11-03 23:07:01 +0100

    Update city.js
    Update of API key credentials

--- calibre_frontend_mvp/screens/flows/newSignUpFlow/city.js
+++ calibre_frontend_mvp/screens/flows/newSignUpFlow/city.js
@@ -19,7 +19,7 @@ const windowHeight = Dimensions.get('window').height;
 export default ({ route, navigation }) => {
    
 
-    const GOOGLE_API_KEY = 'AIzaSyC1DL8gnppq5oNaBExpRynw-VI2_zGKkQM';
+    const GOOGLE_API_KEY = 'AIzaSyBHtU7_napulWRahIUk6D1a5SW4iZwZeWM';
 
 
     const [buttonState, setButtonState] = React.useState(true);
