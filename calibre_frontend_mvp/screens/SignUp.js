b9348c95f95869726cb6e090d9b2f1d908e3e578
Adam Isaac <44964494+Wistoft2410@users.noreply.github.com>
2026-08-12 14:23:19 +0200

    Cleaning up

--- calibre_frontend_mvp/screens/SignUp.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Button } from 'react-native';
-
-export default ({ navigation }) => (
-  <>
-    <Button title="Sign Out" onPress={() => alert('todou!')} />
-    <Button title="Sign Up" onPress={() => navigation.push('Sign up')} />
-  </>
-);
